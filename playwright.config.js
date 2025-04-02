fd99cb8ca95e1edcc7386e1b9c14f0b84b0eb0af
Mindfire-Tanmay <tanmaym@mindfiresolutions.com>
2025-04-02 10:51:44 +0530

    Fix config and reporting

--- playwright.config.js
+++ playwright.config.js
@@ -24,8 +24,8 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
-    ['junit', { outputFile: 'test-results/${process.env.BROWSER}/results.xml' }],  // JUnit per browser
-    ['html', { outputFolder: 'playwright-report/${process.env.BROWSER}' }],  // HTML per browser
+    ['html', { outputFolder: `playwright-report/${process.env.BROWSER}` }],
+    ['junit', { outputFile: `test-results/${process.env.BROWSER}/results.xml` }]
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
