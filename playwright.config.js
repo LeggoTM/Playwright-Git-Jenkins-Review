dbb8832ccb9a0f5bfa58a6186c4a377b87cf1ef2
Mindfire-Tanmay <tanmaym@mindfiresolutions.com>
2025-04-02 10:22:01 +0530

    Prepare jenkins pipeline for parallel

--- playwright.config.js
+++ playwright.config.js
@@ -24,8 +24,8 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
-    ['html', { outputFolder: 'playwright-report' }],
-    ['junit', { outputFile: 'results.xml' }]
+    ['junit', { outputFile: 'test-results/${process.env.BROWSER}/results.xml' }],  // JUnit per browser
+    ['html', { outputFolder: 'playwright-report/${process.env.BROWSER}' }],  // HTML per browser
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
